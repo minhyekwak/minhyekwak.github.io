5c13626f339920781b6a0d35988a7e5eaeca8e81
곽민혜 <kwak.minhye@gmail.com>
2017-10-11 14:57:24 -0400

    javascript 1st assignment

--- javascript/main.js
+++ javascript/main.js
@@ -22,19 +22,63 @@ function menuFunction2(){
 }
 menuFunction2();
 
-//var coffeeImg = ['image/coffee1.jpg', 'image/coffee2.jpg', 'image/coffee3.jpg', 'image/coffee4.jpg', 'image/coffee5.jpg']
-//function coffeeImage(){
-//  $('.coffee').attr('src', coffeeImg)
-//  function choice(arr){
-//  return arr[Math.floor(Math.random() * arr.length)]
-//}
-//}
-//coffeeImage();
-
-var markov = new RiMarkov(1);
-function generateQuote(numSentences){
- $('.quote span').text(markov.generateSentences(numSentences).join(' '))
+var coffeeImg = ['image/coffee1.jpg', 'image/coffee2.jpg', 'image/coffee3.jpg', 'image/coffee4.jpg', 'image/coffee5.jpg']
+function coffeeImage(){
+  $('.coffee').attr('src', choice(coffeeImg))
+  function choice(arr){
+  return arr[Math.floor(Math.random() * arr.length)]
+  }
 }
-var text = 'As long as there was coffee in the world, how bad could things be? I would rather take coffee than compliments just now. Coffee is a way of stealing time that should by rights belong to your older self.'
-markov.loadText(text)
-generateQuote(1);
+coffeeImage();
+
+//var markov = new RiMarkov(2);
+//function generateQuote(numSentences){
+// $('.quote span').text(markov.generateSentences(numSentences).join(' '))
+//}
+//markov.loadText(text)
+//generateQuote(2);
+
+var coffeeQuotes = ['As long as there was coffee in the world, how bad could things be?', 'I would rather take coffee than compliments just now.', 'Coffee is a way of stealing time that should by rights belong to your older self.', 'Black as night, sweet as sin.', 'Even bad coffee is better than no coffee at all.', 'Adventure in life is good; consistency in coffee even better.', 'If it was not for the coffee, I would have no identifiable personality whatsover.']
+var quotes = choice(coffeeQuotes)
+$('.quote span').text(quotes)
+function choice(arr){
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
+var coffeeTyp = ['image/types-01-latte.jpg', 'image/types-02-americano.jpg', 'image/types-03-flatwhite.jpg', 'image/types-04-mocha.jpg', 'image/types-05-espresso.jpg', 'image/types-06-cappuchino.jpg']
+function coffeeTypes(){
+  $('.imgTypes').attr('src', choice(coffeeTyp))
+  function choice(arr){
+  return arr[Math.floor(Math.random() * arr.length)]
+  }
+}
+coffeeTypes();
+
+var coffeeRec = ['image/recipe-01.jpg', 'image/recipe-02.jpg', 'image/recipe-03.jpg', 'image/recipe-04.jpg', 'image/recipe-05.jpg', 'image/recipe-06.jpg', 'image/recipe-07.jpg', 'image/recipe-08.jpg', 'image/recipe-09.jpg', 'image/recipe-10.jpg', 'image/recipe-11.jpg', 'image/recipe-12.jpg']
+function coffeeRecipes(){
+     var x = Math.floor((Math.random() * coffeeRec.length));
+    document.getElementById('imgRecipes').src = coffeeRec[x];
+}
+
+var coffeeSho = ['image/cafe-01.jpg', 'image/cafe-02.jpg', 'image/cafe-03.jpg', 'image/cafe-04.jpg', 'image/cafe-05.jpg']
+function coffeePlaces(){
+    var y = Math.floor((Math.random() * coffeeSho.length));
+    document.getElementById('imgCafes').src = coffeeSho[y];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
